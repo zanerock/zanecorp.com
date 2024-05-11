ca4ddf8119f62a502e313bf70f9bb81d07230092
Zane Rockenbaugh <zane@liquid-labs.com>
2024-05-11 13:54:54 -0500

    some initial tinkering on the front page

---
+++ src/pages/index.js
@@ -0,0 +1,38 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import NoNavLayout from '../components/NoNavLayout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header style={{ height: '100vh', padding: '2rem 0' }}>
+      <div style={{ height: 'calc(100vh - 4rem)', display: 'flex', justifyContent: 'center' }} className="container">
+        <div className={clsx(styles.logoAndNavContainer)}>
+          <img className={clsx(styles.logo)} 
+            src="/img/zanecorp-logo.svg"
+            alt="Zanecorp logo showing large Z above stylized 'ZANECORP'" />
+          <Link className={clsx('button', styles.navButton)} to=".">Reviews</Link>
+          <div className={clsx(styles.navButton)}>Reviews</div>
+          <div className={clsx(styles.navButton)}>Blog</div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <NoNavLayout
+      wrapperClassName='homePage'
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+    </NoNavLayout>
+  );
+}
