cd5ac79e5c51ee5ecbaa0ea6fe6c35bbc4150804
Zane Rockenbaugh <zane@liquid-labs.com>
2024-05-11 16:04:33 -0500

    removed unecessary imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import NoNavLayout from '../components/NoNavLayout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
