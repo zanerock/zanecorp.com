46336d37439587926e60a797957d4afbcc06e71f
Zane Rockenbaugh <zane@liquid-labs.com>
2024-06-01 09:49:19 -0500

    changed siebar ID to match purpose

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'reviewsSidebar',
             position: 'left',
             label: 'Reviews',
           },
